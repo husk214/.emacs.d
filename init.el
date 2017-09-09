afa5a5c60742b5ce65c610848ac91bdba55155f5
husk214 <shibagaki.a.mllab.nit@gmail.com>
2017-09-10 03:03:25 +0900

    update

--- init.el
+++ init.el
@@ -1,5 +1,15 @@
-(require 'package)
+;;; init.el --- My emacs init  file  -*- lexical-binding: t -*-
 
+;; Filename: init.el
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'package)
+(eval-when-compile (require 'cl))
+
+;;; Code
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
@@ -14,6 +24,9 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(line-number-mode t)
+(column-number-mode t)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -43,10 +56,7 @@
                       '(lambda () (flyspell-mode 1))))
    '(
      yatex-mode-hook
-
      ))
-
-(require 'cl)
 
 (defvar installing-package-list
   '(
@@ -60,20 +70,23 @@
     magit
     multiple-cursors
     smartrep
+    helm
+    helm-tramp
     company
     python-mode
     jedi
     virtualenvwrapper
     epc
-    python
+    python-mode
     company-jedi
+    flycheck
+    helm-flycheck
+    py-yapf
     irony
     company-irony
     company-irony-c-headers
     clang-format
     yasnippet
-    helm
-    helm-tramp
     auto-complete
     rainbow-delimiters
     fuzzy
@@ -87,38 +100,14 @@
     (dolist (pkg not-installed)
         (package-install pkg))) )
 
-(require 'madhat2r-theme)
-(load-theme 'madhat2r t)
-
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-(require 'igrep)
-(igrep-define lgrep (igrep-use-zgrep 4nil)(igre-regex-option "-n -Ou8"))
-(igrep-find-define lgrep (igrep-use-zgrep nil)(igrep-regex-option "-n -Ou8"))
-
-(use-package git-gutter
-  :ensure t
-  :config
-  (global-git-gutter-mode t))
-
-(use-package magit
-  :ensure t
-  :config
-  (global-set-key (kbd "C-x g") 'magit-status))
-
 (require 'multiple-cursors)
 (require 'smartrep)
-
 (declare-function smartrep-define-key "smartrep")
-
 (global-set-key (kbd "C-M-c") 'mc/edit-lines)
 (global-set-key (kbd "C-M-r") 'mc/mark-all-in-region)
-
-(global-unset-key "\C-r")
-
-(smartrep-define-key global-map "C-r"
-  '(("C-r"      . 'mc/mark-next-like-this)
+(global-unset-key "\C-t")
+(smartrep-define-key global-map "C-t"
+  '(("C-t"      . 'mc/mark-next-like-this)
     ("n"        . 'mc/mark-next-like-this)
     ("p"        . 'mc/mark-previous-like-this)
     ("m"        . 'mc/mark-more-like-this-extended)
@@ -132,83 +121,127 @@
     ("o"        . 'mc/sort-regions)
     ("O"        . 'mc/reverse-regions)))
 
-  (use-package company
-    ;; complete anything
-    :ensure t
-    :init
-    (add-hook 'after-init-hook 'global-company-mode)
-    (add-hook 'c-mode-common-hook 'company-mode)
-    (add-hook 'emacs-lisp-mode-hook 'company-mode)
-    :config
-    (setq company-async-timeout 0.5) ;; set timeout to 10 seconds
-    ;;(setq company-idle-delay 0.1)
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (define-key company-search-map (kbd "C-n") 'company-select-next)
-    (define-key company-search-map (kbd "C-p") 'company-select-previous)
-    (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-    (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+(use-package madhat2r-theme
+  :ensure t
+  :init
+  (load-theme 'madhat2r t)
+  )
 
-    (set-face-attribute 'company-tooltip nil
-			:foreground "black" :background "lightgrey")
-    (set-face-attribute 'company-tooltip-common nil
-			:foreground "black" :background "lightgrey")
-    (set-face-attribute 'company-tooltip-common-selection nil
-			:foreground "white" :background "steelblue")
-    (set-face-attribute 'company-tooltip-selection nil
-			:foreground "black" :background "steelblue")
-    (set-face-attribute 'company-preview-common nil
-			:background nil :foreground "lightgrey" :underline t)
-    (set-face-attribute 'company-scrollbar-fg nil
-			:background "orange")
-    (set-face-attribute 'company-scrollbar-bg nil
-			:background "gray40")
-    )
-  (use-package jedi
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode)
+  )
+
+(use-package igrep
+  :ensure t
+  :config
+  (igrep-define lgrep (igrep-use-zgrep 4nil)(igre-regex-option "-n -Ou8"))
+  (igrep-find-define lgrep (igrep-use-zgrep nil)(igrep-regex-option "-n -Ou8"))
+  )
+
+(use-package git-gutter
+  :ensure t
+  :config
+  (global-git-gutter-mode t)
+  )
+
+(use-package magit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status)
+  )
+
+(use-package company
+  ;; complete anything
+  :defer t
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  (add-hook 'c-mode-common-hook 'company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
+  :config
+  (setq company-async-timeout 0.5) ;; set timeout to 10 seconds
+  ;;(setq company-idle-delay 0.1)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+
+  (set-face-attribute 'company-tooltip nil
+		      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common nil
+		      :foreground "black" :background "lightgrey")
+  (set-face-attribute 'company-tooltip-common-selection nil
+		      :foreground "white" :background "steelblue")
+  (set-face-attribute 'company-tooltip-selection nil
+		      :foreground "black" :background "steelblue")
+  (set-face-attribute 'company-preview-common nil
+		      :background nil :foreground "lightgrey" :underline t)
+  (set-face-attribute 'company-scrollbar-fg nil
+		      :background "orange")
+  (set-face-attribute 'company-scrollbar-bg nil
+		      :background "gray40")
+  )
+
+(use-package jedi
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'python-mode-hook 'jedi:setup)
+  :config
+  (setq jedi:complete-on-dot t)
+  )
+(use-package virtualenvwrapper
+  :ensure t
+  :defer t
+  )
+(use-package irony
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'c-mode-common-hook 'irony-mode)
+  :config
+  ;; replace completion-at-point and complete-symbol by irony-mode's asynchronous functions
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  (setq irony-additional-clang-options '(;; use C++11
+					 "-std=c++11"))
+  (use-package company-irony
     :ensure t
-    :defer t
     :init
-    (add-hook 'python-mode-hook 'jedi:setup)
-    :config
-    (setq jedi:complete-on-dot t)
+    (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+    (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
     )
-  (use-package virtualenvwrapper
+  ;; enable C/C++ header completion
+  (use-package company-irony-c-headers
     :ensure t
-    :defer t
     :config
-    (setenv "PYTHONPATH" "/Users/shibagaki/.local/share/virtualenvs/python-DRMAozEm")
-    )
-  (use-package irony
-    :ensure t
-    :defer t
-    :init
-    (add-hook 'c-mode-common-hook 'irony-mode)
-    :config
-    ;; replace completion-at-point and complete-symbol by irony-mode's asynchronous functions
-    (defun my-irony-mode-hook ()
-      (define-key irony-mode-map [remap completion-at-point]
-  'irony-completion-at-point-async)
-      (define-key irony-mode-map [remap complete-symbol]
-  'irony-completion-at-point-async))
-    (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-    (setq irony-additional-clang-options '(;; use C++11
-             "-std=c++11"))
-    (use-package company-irony
-      :ensure t
-      :init
-      (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-      (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
-      )
-    ;; enable C/C++ header completion
-    (use-package company-irony-c-headers
-      :ensure t
-      :config
-      (eval-after-load 'company '(add-to-list 'company-backends '(company-irony-c-headers company-irony)))) )
+    (eval-after-load 'company '(add-to-list 'company-backends '(company-irony-c-headers company-irony)))) )
+
+(use-package flycheck
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode) )
+
+(use-package py-yapf
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'python-mode-hook 'py-yapf-enable-on-save) )
 
 (load-file "$HOME/.emacs.d/google-c-style.el")
 (use-package google-c-style
   :ensure t
+  :defer t
   :init
   (add-hook 'c-mode-common-hook 'google-set-c-style)
   (add-hook 'c++-mode-common-hook 'google-set-c-style)
@@ -216,37 +249,26 @@
 
 (use-package clang-format
   :ensure t
+  :defer t
   :config
   (require 'clang-format)
   (global-set-key (kbd "C-c j") 'clang-format-region)
   (global-set-key (kbd "C-c u") 'clang-format-buffer)
   (setq clang-format-style-option "google") )
 
-(eval-after-load "yasnippet"
-  '(progn
-     (define-key yas-keymap (kbd "<tab>") nil)
-     (define-key yas-keymap (kbd "C-c i") nil)
-     (define-key yas-minor-mode-map [(tab)] nil)
-     (define-key yas-minor-mode-map (kbd "TAB") nil)
-     (define-key yas-keymap (kbd "C-x j") 'yas/expand)
-     (define-key yas-minor-mode-map (kbd "C-x j") 'yas-insert-snippet)
-     (yas-global-mode 1)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (helm-tramp helm anything imenus imenu-list dumb-jump yatex yasnippet-bundle yasnippet use-package undo-tree smartrep seti-theme python-mode powerline package-utils multiple-cursors magit irony-eldoc igrep grep-a-lot goto-chg google-c-style git-gutter-fringe fzf flycheck-irony company-jedi company-irony-c-headers company-irony clang-format auto-complete afternoon-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :bind (:map yas-minor-mode-map
+              ("C-x i i" . yas-insert-snippet)
+              ("C-x i n" . yas-new-snippet)
+              ("C-x i v" . yas-visit-snippet-file)
+              ("C-x i l" . yas-describe-tables)
+              ("C-x i g" . yas-reload-all))
+  :config
+  (yas-global-mode 1)
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  )
 
 (require 'helm-config)
 (helm-mode 1)
@@ -258,6 +280,7 @@
 (define-key global-map (kbd "C-x b")   'helm-buffers-list)
 (define-key global-map (kbd "C-;") 'helm-mini)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map "\C-h" 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
@@ -319,6 +342,14 @@
 ;; Connect tramp with bash
 (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
 
+(use-package helm-flycheck
+  :ensure t
+  :defer t
+  :commands (flycheck)
+  :bind ("C-x 4 h" . helm-flycheck)
+  )
+
+
 (require 'auto-complete-config)
 (ac-config-default)
 (add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
@@ -326,9 +357,23 @@
 (add-to-list 'ac-modes 'org-mode)
 (add-to-list 'ac-modes 'yatex-mode)
 (ac-set-trigger-key "TAB")
-(setq ac-delay 0.2)  
+(setq ac-delay 0.2)
 (setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
 (setq ac-use-fuzzy t)          ;; 曖昧マッチ
 
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yatex yasnippet use-package undo-tree smartrep seti-theme rainbow-delimiters python-mode pyenv-mode-auto py-yapf powerline package-utils multiple-cursors magit madhat2r-theme jedi irony-eldoc imenus imenu-list igrep helm-tramp helm-flycheck grep-a-lot goto-chg google-c-style git-gutter-fringe fzf fuzzy flycheck-irony exec-path-from-shell dumb-jump company-jedi company-irony-c-headers company-irony clues-theme clang-format auto-virtualenvwrapper anything afternoon-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
